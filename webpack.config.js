e044a4310545f0583c3764dd111ec54377ac6f0e
yosefHesham <yousefmobdev@gmail.com>
2022-05-04 17:41:05 +0200

    Finish setup-web-pack

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,33 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode:"development",
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  devtool: 'inline-source-map',
+
   plugins: [
-    new HtmlWebpackPlugin({title:"Output management"})
+    new HtmlWebpackPlugin({title:"Development",
+   template: './src/index.html',
+
+  })
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    clean:true
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
